c9424b765962c0a3e0623cc597b53d260282bc82
Byron <gordon.byron.lien@gmail.com>
2018-12-11 00:01:34 +0800

    Add answer of FrogJmp.js

---
+++ Codility/Lesson 3/FrogJmp.js
@@ -0,0 +1,19 @@
+// https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
+// Task Score: 100
+// Correctness: 100
+// Performance: 100, O(1)
+
+function solution(X, Y, D) {
+  // If no need to move
+  if (X === Y) {
+    return 0;
+  }
+
+  // If only need one move
+  if (Y - X <= D) {
+    return 1;
+  }
+
+  // Other cases
+  return Math.ceil((Y - X) / D);
+}
