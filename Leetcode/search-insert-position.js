655fd51c7b7d2939c650c2989d1c6f5295c77a8a
Byron <gordon.byron.lien@gmail.com>
2019-03-07 16:08:32 +0800

    Add answer of search-insert-position.js

---
+++ Leetcode/search-insert-position.js
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/search-insert-position/
+// Runtime: 56 ms, faster than 80.73% of JavaScript online submissions for Search Insert Position.
+// Memory Usage: 33.8 MB, less than 55.03% of JavaScript online submissions for Search Insert Position.
+
+// TO-DO use binary search
+
