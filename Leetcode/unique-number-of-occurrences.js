648b0f3b2eb4d15086e45092ca80fa90779e56bd
Byron <gordon.byron.lien@gmail.com>
2020-05-23 23:24:46 +0900

    Add answer of unique-number-of-occurrences.js

---
+++ Leetcode/unique-number-of-occurrences.js
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/unique-number-of-occurrences/
+// Runtime: 52 ms, faster than 81.87% of JavaScript online submissions for Unique Number of Occurrences.
+// Memory Usage: 34.8 MB, less than 100.00% of JavaScript online submissions for Unique Number of Occurrences.
+
+/*
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+
+var uniqueOccurrences = function (arr) {
+  if (arr.length === 1) return true;
+  if (arr.length === 2) {
+    if (arr[0] !== arr[1]) return false;
+    return true;
+  }
+
+  // Input: arr = [1,2,2,1,1,3]
+  // Output: arr = [1,1,1,2,2,3]
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+
+  // Count until number changed
+  // Check the count of number exists in result array or not
+  // If exist then return false
+  // If not then push into result
+  let preEl = arr[0];
+  let count = 1;
+  let result = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === preEl) {
+      count++;
+    } else {
+      if (result.indexOf(count) !== -1) return false;
+      result.push(count);
+      preEl = arr[i];
+      count = 1;
+    }
+  }
+
+  return true;
+};
