9679e85e73bf91972e7eeca6b5600d3d71629230
Byron <gordon.byron.lien@gmail.com>
2020-08-01 12:22:52 +0900

    Add answer of running-sum-of-1d-array.js

---
+++ Leetcode/running-sum-of-1d-array.js
@@ -0,0 +1,18 @@
+// Runtime: 68 ms, faster than 90.48% of JavaScript online submissions for Running Sum of 1d Array.
+// Memory Usage: 37.4 MB, less than 100.00% of JavaScript online submissions for Running Sum of 1d Array.
+
+/*
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var runningSum = function (nums) {
+  const len = nums.length;
+  if (len < 2) return nums;
+
+  for (let i = 0; i < len - 1; i++) {
+    nums[i + 1] += nums[i];
+  }
+
+  return nums;
+}
