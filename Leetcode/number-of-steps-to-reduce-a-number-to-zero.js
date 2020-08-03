305ac5c8c107a171118d942688b8d125128f08db
Byron <gordon.byron.lien@gmail.com>
2020-08-03 16:36:57 +0900

    Add answer of number-of-steps-to-reduce-a-number-to-zero.js

---
+++ Leetcode/number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/number-of-steps-to-reduce-a-number-to-zero/
+/*
+ * @param {number} num
+ * @return {number}
+ */
+
+ /*
+  recursive
+
+
+  var numberOfSteps  = function(num) {
+    if(num == 0) return 0;
+    return 1 + (num % 2 === 0 ? numberOfSteps(num / 2) : numberOfSteps(num - 1));
+  }
+ */
+
+var numberOfSteps  = function(num) {
+  if(num == 0) return 0;
+
+  let count = 1;
+
+  while(num > 1){
+    num = num % 2 === 0 ? num / 2 : num -1;
+    count++;
+  }
+
+  return count;
+};
