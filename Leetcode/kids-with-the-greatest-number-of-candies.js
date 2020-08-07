1e8e2133f02a4b882959ed479033b9b45221de26
Byron <gordon.byron.lien@gmail.com>
2020-08-07 18:24:06 +0900

    Add answer of kids-with-the-greatest-number-of-candies.js

---
+++ Leetcode/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,19 @@
+// // https://leetcode.com/kids-with-the-greatest-number-of-candies/
+// Runtime: 84 ms, faster than 28.39% of JavaScript online submissions for Kids With the Greatest Number of Candies.
+// Memory Usage: 36.3 MB, less than 18.55% of JavaScript online submissions for Kids With the Greatest Number of Candies.
+
+/*
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+
+var kidsWithCandies = function (candies, extraCandies) {
+  const maximum = Math.max(...candies);
+
+  for (let i = 0; i < candies.length; i++) {
+    candies[i] = candies[i] + extraCandies >= maximum ? true : false;
+  }
+
+  return candies;
+};
