37161703eaed04cb344766ea9da380e7dbd02865
Lian <gordon.byron.lien@gmail.com>
2020-10-26 15:00:29 +0900

    Add answer of add-to-array-form-of-integer.js

---
+++ Leetcode/add-to-array-form-of-integer.js
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/add-to-array-form-of-integer/
+// Runtime: 132 ms, faster than 53.77% of JavaScript online submissions for Add to Array-Form of Integer.
+// Memory Usage: 46.2 MB, less than 5.53% of JavaScript online submissions for Add to Array-Form of Integer.
+
+/*
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number[]}
+ */
+
+var addToArrayForm = function (A, K) {
+  // One line but performance is not good
+  // return (BigInt(A.join('')) + BigInt(K)).toString().split('');
+
+  const arrK = K.toString().split("");
+  let result = [];
+  const shortArr = arrK.length > A.length ? A : arrK;
+  const longArr = arrK.length > A.length ? arrK : A;
+
+  let j = shortArr.length - 1;
+  let carry = 0;
+  for (let i = longArr.length - 1; i >= 0; i--) {
+    if (shortArr[j] === undefined) {
+      if (carry === 0) break; // For performance
+      shortArr[j] = 0;
+    }
+    
+    let tmp = parseInt(longArr[i]) + parseInt(shortArr[j]) + carry;
+    if (tmp > 9) {
+      carry = 1;
+      tmp = tmp - 10;
+    } else {
+      carry = 0;
+    }
+
+    result.unshift(tmp);
+    j--;
+  }
+
+  if (carry !== 0) result.unshift(carry);
+
+  if (result.length < longArr.length)
+    result = longArr.slice(0, longArr.length - result.length).concat(result);
+
+  return result;
+};
