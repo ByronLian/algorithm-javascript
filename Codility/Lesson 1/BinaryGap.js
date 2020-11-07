ea96cc59cba9bcbfd4713f61ec0940adc890378d
Byron <gordon.byron.lien@gmail.com>
2020-11-07 16:42:27 +0900

    Update answer of BinaryGap.js

--- Codility/Lesson 1/BinaryGap.js
+++ Codility/Lesson 1/BinaryGap.js
@@ -4,41 +4,26 @@
 // Performance: N/A
 
 function solution(N) {
-  let biNum = (N).toString(2); // Turn N to binary number
+  const binaryStr = (N >>> 0).toString(2); // Turn N to binary number
 
   // Return when N only has one binary number
-  let biNumLen = biNum.length;
-  if (biNumLen === 1) {
-    return 0;
-  }
+  const len = binaryStr.length;
+  if (binaryStrLen === 1) return 0;
 
-  let longestStr = "1";         // For storing longest number
-  let longestStrZeroCount = 0;  // For storing '0' length of longest number
-  let tempZero = 0;
-  let tempStr = "1";
-  
+  let longestCount = 0;  // For storing '0' length of longest number
+  let count = 0;
+
   // e.g 1041 = 10000010001
   // start from left to right and add '0' into temp string until next 1 showup
-  // compare temp string and longest string so far
-  // swap if the temp string is longer then current longest string
-  // return longest string
-  
-  for (let i = 1; i < biNumLen; i++) {
-    if (biNum[i] === '0') {
-      tempStr += biNum[i];
-      tempZero ++;
-      continue;
-    }
-
-    if (biNum[i] === '1') {
-      if (tempStr > longestStr) {
-        longestStr = tempStr;
-        longestStrZeroCount = tempZero;
-      }
-      tempStr = "1";
-      tempZero = 0;
+  // compare longestCount and count
+  for (let i = 1; i < len; i++) {
+    if (binaryStr[i] === '0') {
+      count++;
+    } else {
+      if (count > longestCount) longestCount = count;
+      count = 0
     }
   }
 
-  return longestStrZeroCount;
+  return longestCount;
 }
