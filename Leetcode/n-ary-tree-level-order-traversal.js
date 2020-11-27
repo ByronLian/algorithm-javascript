706e54060d8995b5eaf2e6ce94d2a1d4c3709f4c
Lian <gordon.byron.lien@gmail.com>
2020-11-27 10:58:01 +0900

    Add answer of n-ary-tree-level-order-traversal.js

---
+++ Leetcode/n-ary-tree-level-order-traversal.js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/n-ary-tree-level-order-traversal/
+// Runtime: 88 ms, faster than 98.02% of JavaScript online submissions for N-ary Tree Level Order Traversal.
+// Memory Usage: 43.6 MB, less than 29.70% of JavaScript online submissions for N-ary Tree Level Order Traversal.
+
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/*
+ * @param {Node} root
+ * @return {number[][]}
+ */
+
+var levelOrder = function (root) {
+  let result = [];
+
+  let traverse = (node, lv) => {
+    if (!node) return;
+
+    result?.[lv] ? result[lv].push(node.val) : (result[lv] = [node.val]);
+
+    for (let i in node.children) {
+      traverse(node.children[i], lv + 1);
+    }
+  };
+
+  traverse(root, 0);
+  return result;
+};
