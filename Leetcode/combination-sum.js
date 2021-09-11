8682ae7b0e70ae91f992ed6816c18cb255d718f6
Byron <gordon.byron.lien@gmail.com>
2021-09-11 23:25:31 +0900

    Add answer of combination-sum.js

---
+++ Leetcode/combination-sum.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/combination-sum/
+// Runtime: 80 ms, faster than 95.83% of JavaScript online submissions for Combination Sum.
+// Memory Usage: 45.5 MB, less than 20.84% of JavaScript online submissions for Combination Sum.
+
+/*
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+
+var combinationSum = function (candidates, target) {
+  const result = [];
+  const len = candidates.length;
+
+  const checkAndPick = (arr, sum, start) => {
+    if (sum > target || start > len - 1) return;
+
+    if (sum === target) return result.push(arr);
+
+    for (let i = start; i < len; i++) {
+      arr.push(candidates[i]);
+      checkAndPick([...arr], sum + candidates[i], i);
+      arr.pop(); // backtrack
+    }
+  }
+
+  checkAndPick([], 0, 0);
+  return result;
+};
